4d9f1cc56af8499a6d73f2791c85091c4a9d7b04
nabunjeshamim <149717412+nabunjeshamim@users.noreply.github.com>
2024-02-07 12:55:53 -0800

    Add files via upload

---
+++ Shamim vs code/index.js
@@ -0,0 +1,17 @@
+
+
+
+
+function validateCredentials(email,password){
+const validEmail ="Nabunjeshamim@gmail.com";
+const validPassword ="22/U/6491";
+if (email === validEmail && password === validPassword) {
+    console.log(`User email is $ {email}, login successfully!`);
+}else{
+    console.log ("correct user credentials");
+    console.log("incorrect user credentials!");
+
+}
+
+}
+validateCredentials();
